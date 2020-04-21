b0bf1bdf07d5322ae838fc4e9337e03befac5462
gesab001 <144000giovanni@gmail.com>
2020-04-21 14:07:33 +1200

    updated xmls

---
+++ updateNews.py
@@ -0,0 +1,17 @@
+import json
+import subprocess
+
+f = open("news.json")
+json_string = f.read()
+rss = json.loads(json_string)
+for news in rss:
+   url= rss[news]
+   print(url)
+   try:
+    command = "curl '" + url + "' -o ../html/headlines.onecloudapps.net/" + news + ".xml"
+    subprocess.call(command, shell=True)
+    print(news+".xml saved successfully" )
+   except Exception as ex:
+    print(ex)
+   
+   
