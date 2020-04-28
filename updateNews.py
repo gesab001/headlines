bd343a2649a54cfba9a8eaa2a30f315144fe12f8
Giovanni Saberon <144000giovanni@gmail.com>
2020-04-28 13:27:04 +1200

    updated xml

--- updateNews.py
+++ updateNews.py
@@ -20,26 +20,3 @@ command = "sudo python3 updateDate.py"
 subprocess.call(command, shell=True)
 command = "sudo ./gitupdater.sh"
 subprocess.call(command, shell=True)
-#!/usr/bin/python3
-import json
-import subprocess
-
-f = open("news.json")
-json_string = f.read()
-rss = json.loads(json_string)
-for news in rss:
-   url= rss[news]
-   print(url)
-   try:     
-    command = "sudo curl -L '" + url + "' -o " + news + ".xml"
-    subprocess.call(command, shell=True)
-    
-    print(news+".xml saved successfully" )
-   except Exception as ex:
-    print(ex)
-
-command = "sudo python3 updateDate.py"
-subprocess.call(command, shell=True)
-command = "sudo ./gitupdater.sh"
-subprocess.call(command, shell=True)
-
