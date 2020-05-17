a66b4e604cc9106f81a4ece5db604323329da09a
gesab001 <144000giovanni@gmail.com>
2020-05-18 11:25:43 +1200

    updated xml

--- gitupdater.sh
+++ gitupdater.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cd /var/www/html/headlines/
-sudo /usr/bin/git pull
-sudo /usr/bin/git add .
-sudo /usr/bin/git commit -m "updated xml"
-sudo /usr/bin/git push --all
+cd /var/www/html/headlines/ 
+sudo git pull 
+sudo git add . 
+sudo  git commit -m "updated xml" 
+sudo  git push --all
