5bf83810717629941a1b528e4edf878bf3fc2a1e
giovanni <144000giovanni@gmail.com>
2020-05-18 11:52:31 +1200

    updated xml

--- gitupdater.py
+++ gitupdater.py
@@ -1,17 +1,2 @@
-#!/usr/bin/python
-
-
-
-from git import Repo,remote
-
-rw_dir = 'var/www/html/headlines'
-repo = Repo(rw_dir)
-
-'''Enter code to commit the repository here.
-After commit run the following code to push the commit to remote repo.
-I am pushing to master branch here'''
-
-origin = repo.remote(name='origin')
-origin.push()
-
-
+#!/usr/bin/python3
+import pygit
