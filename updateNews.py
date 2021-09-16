03fa69da2ea85fbecda9b183d33686a32e68f040
gesab001 <144000giovanni@gmail.com>
2021-09-17 11:57:01 +1200

    update news

--- updateNews.py
+++ updateNews.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 import json
 import subprocess
+import platform
 
-f = open("/var/www/html/headlines/news.json")
+f = open("news.json")
 json_string = f.read()
 rss = json.loads(json_string)
 for news in rss:
    url= rss[news]
    print(url)
    try:     
-    command = "sudo curl -L '" + url + "' -o /var/www/html/headlines/" + news + ".xml"
+    command = "sudo curl -L '" + url + "' -o " + news + ".xml"
+    if platform.system=="Windows":
+      command = "curl -L '" + url + "' -o " + news + ".xml"
+
     subprocess.call(command, shell=True)
     
     print(news+".xml saved successfully" )
    except Exception as ex:
     print(ex)
 
-command = "sudo python3 updateDate.py"
+command = "python3 updateDate.py"
 subprocess.call(command, shell=True)
+
 command = "./deploy.sh"
+if platform.system=="Windows":
+  command = "deploy"
 subprocess.call(command, shell=True)
